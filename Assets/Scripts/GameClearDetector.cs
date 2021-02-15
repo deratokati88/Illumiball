0655e635039de11f67af62d1dafd85a0580189d5
deratokati88 <74710163+deratokati88@users.noreply.github.com>
2021-02-15 11:52:02 +0900

    first commit

---
+++ Assets/Scripts/GameClearDetector.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameClearDetector : MonoBehaviour
+{
+    public Hole holeRed;
+    public Hole holeBlue;
+    public Hole holeGreen;
+
+    private void OnGUI()
+    {
+        GUI.matrix = Matrix4x4.Scale(Vector3.one * 2);
+        if(holeRed.IsHoldind() && holeBlue.IsHoldind() && holeGreen.IsHoldind())
+        {
+            Debug.Log("ゲームくりあ");
+            GUI.Label(new Rect(50, 50, 100, 30), "GameClear!");
+        }
+    }
+
+}
